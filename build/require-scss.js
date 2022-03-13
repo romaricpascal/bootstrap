8a9e5c11155036a604733f4795654534146448b8
Romaric Pascal <hello@romaricpascal.is>
2022-03-13 16:44:20 +0000

    Remove unnecessary computation of sass-true path
    Sass seems to actually resolve 'true' as 'sass-true' within node modules OK

--- build/require-scss.js
+++ build/require-scss.js
@@ -2,18 +2,14 @@
 
 /* eslint-env node */
 
-const path = require('path')
-
 require.extensions['.scss'] = function (module, filename) {
-  const sassTruePath = path.relative(path.dirname(filename), process.cwd()) + '/node_modules/sass-true'
-
   return module._compile(`
 var sassTrue = require('sass-true');
 var fs = require('fs');
 var path = require('path');
 var filename = '${filename}';
 var data = fs.readFileSync(filename, {encoding: 'utf-8'});
-var TRUE_SETUP = "$true-terminal-output: false; @import '${sassTruePath}';";
+var TRUE_SETUP = "$true-terminal-output: false; @import 'true';";
 try {
   sassTrue.runSass({
     data: TRUE_SETUP + data,
